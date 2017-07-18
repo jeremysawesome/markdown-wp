ca106c423ee489e283265ef15bd2e38c243b7268
Jeremy Smith <jeremy@jeremysawesome.com>
2017-07-18 11:24:06 -0700

    Add plugin file with header

---
+++ markdown-wp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Markdown for WordPress
+ * Plugin URI: https://github.com/jeremysawesome/markdown-wp
+ * Description: Adds a Markdown editor and preview pane when writing posts in WordPress.
+ * Text Domain: awesome-markdown-wp
+ * Version: 0.0.1
+ * Author: Jeremy Smith
+ * Author URI: http://jeremysawesome.com
+ */
